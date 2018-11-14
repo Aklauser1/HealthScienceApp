7058b010194538d5ebaf76fcee6b5a468861a57f
Aklauser1 <31425004+Aklauser1@users.noreply.github.com>
2018-11-14 13:19:55 -0600

    init

---
+++ HealthScienceApp/HealthScienceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthScienceApp
+//
+//  Created by Brandon Schmidt on 11/14/18.
+//  Copyright © 2018 Drew Klauser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
